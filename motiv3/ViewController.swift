7d2042e8fac913b74a104b1ca6022d768b4b2173
Riya Bhivare <riyab@outlook.com>
2020-07-30 11:52:12 -0400

    Initial Commit

---
+++ motiv3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  motiv3
+//
+//  Created by Riya Bhivare on 7/30/20.
+//  Copyright © 2020 Riya Bhivare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
